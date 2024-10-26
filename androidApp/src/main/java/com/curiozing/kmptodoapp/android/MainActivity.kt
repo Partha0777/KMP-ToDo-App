10cf19bec3e03b9d69d3df6c47d70b83da24eb67
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-26 17:09:11 +0530

    Showed title and tasks in the list.

--- androidApp/src/main/java/com/curiozing/kmptodoapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/curiozing/kmptodoapp/android/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.curiozing.kmptodoapp.articles.ToDoViewModel
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +34,18 @@ class MainActivity : ComponentActivity() {
 
                     val toDoList = viewModel.getToDo.collectAsState().value
 
-                    LazyColumn(content = {
+                    LazyColumn(
+                        modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp),
+                        content = {
                         toDoList.forEach {
                             item {
-                                Text(text = it.title)
+                                Column(
+                                    modifier = Modifier.padding(vertical = 12.dp)
+                                ) {
+                                    Text(text = it.title, fontWeight = FontWeight.Bold)
+                                    Spacer(modifier = Modifier.height(12.dp))
+                                    Text(text = it.task)
+                                }
                             }
                         }
                     })
